9722b4cd812a29e07adfffde1bf549aa53ebbabe
Ali Raza <artanha7@gmail.com>
2021-11-06 19:26:08 +0500

    feat: add useful svg icons

--- components/Icon.vue
+++ components/Icon.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg-icon :name="name" :title="title" :desc="desc" class="icon"></svg-icon>
+  <svg-icon :name="name" :title="title" :desc="desc"></svg-icon>
 </template>
 
 <script lang="ts">
@@ -7,8 +7,8 @@ import Vue from 'vue'
 export default Vue.extend({
   props: {
     name: { type: String, required: true },
-    title: { type: String, default: 'test title' },
-    desc: { type: String, default: 'test desc' },
+    title: { type: String, default: null },
+    desc: { type: String, default: null },
   },
 })
 </script>
---
+++ pages/contact.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="contact">
+    <div class="contact__heading">
+      <h1>Contact</h1>
+    </div>
+    <div class="contact__box">
+      <a href="https://twitter.com/username" target="__blank">
+        <icon class="contact__icon" name="twitter"></icon>
+      </a>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ $content }) {
+    const skills = await $content('skills').fetch()
+    return {
+      skills,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/abstracts';
+.contact {
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  flex-direction: column;
+  height: 100%;
+  &__box {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    margin: auto 0;
+    padding: pxToRem(32);
+    max-width: pxToRem(700);
+  }
+  &__icon {
+    width: 2em;
+    height: 2em;
+  }
+}
+</style>
