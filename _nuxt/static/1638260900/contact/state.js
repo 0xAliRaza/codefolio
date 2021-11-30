8e38b772b6df4b03cd900fc41353d22e4b9b2fc7
0xAliRaza <0xAliRaza@users.noreply.github.com>
2021-11-30 08:35:28 +0000

    deploy: 091b96dcf8041f647c2fc36c53b3c90877a1d024

--- _nuxt/static/1638260900/contact/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638260900",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcontact",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"084e2058"}}}}(null));
