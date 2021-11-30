8e3bb357ca5a7cf2df34d6db53faf639e42d78b3
0xAliRaza <0xAliRaza@users.noreply.github.com>
2021-11-30 08:58:21 +0000

    deploy: facf94b2c4e0c69297a2c50a01b3e890cf2ba33a

--- _nuxt/static/1638262282/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638262282",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7962b8a9"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1638262700/portfolio/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638262700",layout:"default",error:a,serverRendered:true,routePath:"\u002Fportfolio",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"67d6e611"}}}}(null));
