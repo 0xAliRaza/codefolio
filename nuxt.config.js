d7ddb7f6077c82e47cbe65608e87fd8671b2a31b
Ali Raza <artanha7@gmail.com>
2021-11-30 14:01:17 +0500

    fix: change root to repository name in nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/codefolio/',
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'codefolio',
