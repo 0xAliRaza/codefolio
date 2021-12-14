fb31e8d57facbc3839640c266d2b91e88f553a8c
0xAliRaza <0xAliRaza@users.noreply.github.com>
2021-12-14 07:13:25 +0000

    deploy: 0a42444646b8cb5d4cf8396a043bf5d19d2e4464

---
+++ _nuxt/static/1639466004/skills/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcodefolio\u002F_nuxt\u002Fstatic\u002F1639466004",layout:"default",error:a,serverRendered:true,routePath:"\u002Fskills",config:{_app:{basePath:"\u002Fcodefolio\u002F",assetsPath:"\u002Fcodefolio\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2495cf67"}}}}(null));
