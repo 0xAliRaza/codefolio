f5c5786080c4a7662f8ee6020afb03c52a1e5ff5
0xAliRaza <0xAliRaza@users.noreply.github.com>
2021-12-14 07:30:08 +0000

    deploy: b992b55b70d584e82538c17f34eb0c8de321e4db

---
+++ _nuxt/static/1639467007/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcodefolio\u002F_nuxt\u002Fstatic\u002F1639467007",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fcodefolio\u002F",assetsPath:"\u002Fcodefolio\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a3f4596e"}}}}(null));
