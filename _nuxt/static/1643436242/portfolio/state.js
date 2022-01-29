491d99339daebea754417bd73a33644bdbd8fd81
0xAliRaza <0xAliRaza@users.noreply.github.com>
2022-01-29 06:53:52 +0000

    deploy: 86a25c1ddda1bc4a6418df84e0b3efc05aebf337

--- _nuxt/static/1643436242/portfolio/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcodefolio\u002F_nuxt\u002Fstatic\u002F1643436242",layout:"default",error:a,serverRendered:true,routePath:"\u002Fportfolio",config:{_app:{basePath:"\u002Fcodefolio\u002F",assetsPath:"\u002Fcodefolio\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"08b40938"}}}}(null));
