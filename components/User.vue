0xAliRaza/codefolio

commit afbad1e25531c1264997a8200eb7191d8bd74bda

feat: Load user image from `asssets/images` if... ... not a valid URL

components/User.vue
@@ -3,7 +3,11 @@
     <div class="user__image-container">
       <img
         class="user__image"
-        :src="user.image"
+        :src="
+          isValidUrl(user.image)
+            ? user.image
+            : require('~/assets/images/' + user.image)
+        "
         :alt="`${user.fullname}'s image`"
       />
     </div>
@@ -35,6 +39,19 @@ export default Vue.extend({
     user: {
       required: true,
       type: Object as () => UserModel,
+    },
+  },
+  methods: {
+    isValidUrl: function (str: string) {
+      let url
+
+      try {
+        url = new URL(str)
+      } catch (_) {
+        return false
+      }
+
+      return url.protocol === 'http:' || url.protocol === 'https:'
     },
   },
 })
@@ -117,12 +134,7 @@ export default Vue.extend({
       line-height: 1.75em;
     }
   }
-  &__learn-more-btn {
-    // padding: 0;
-    // margin: 0;
-    // line-height: inherit;
-    // font-size: inherit;
-  }
+
   &__cta {
     user-select: none;
   }

test/User.spec.ts
@@ -5,7 +5,7 @@ const dummyUser: any = {
   fullname: 'John Doe',
   about:
     "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
-  image: 'source.unsplash.com/random/500x500',
+  image: 'https://source.unsplash.com/random/500x500',
   title: 'some dev',
 }
 const factory = () => {

commit 2f40cc22249af4bfd940b2988d451f18760c990c

fix: Fix strange bug introduced by afbad1e

components/User.vue
@@ -6,7 +6,7 @@
         :src="
           isValidUrl(user.image)
             ? user.image
-            : require('~/assets/images/' + user.image)
+            : require(`~/assets/images/${user.image}`)
         "
         :alt="`${user.fullname}'s image`"
       />